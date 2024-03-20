a174ca5f7dff6aad0e84eeda5451aefde338de13
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-20 17:12:37 +0200

    Remove validation annotations

--- src/main/java/com/example/companydatagateway/entities/TimeSchedule.java
+++ src/main/java/com/example/companydatagateway/entities/TimeSchedule.java
@@ -1,7 +1,5 @@
 package com.example.companydatagateway.entities;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,11 +10,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 public class TimeSchedule implements Serializable {
-
-    @NotNull(message = "The start time can not be null")
     private LocalTime startTime;
-    @NotNull(message = "The end time can not be null")
     private LocalTime endTime;
-    @NotEmpty(message = "The week schedule can not be empty")
     private Set<String> daysOfWeek;
 }
