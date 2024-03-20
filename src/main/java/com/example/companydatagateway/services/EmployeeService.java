723f80bfbbe57147583dc50e4e6ea6b9ad057bce
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-20 17:12:21 +0200

    Remove EmployeeServiceImpl in favor of EmployeeService directly

--- src/main/java/com/example/companydatagateway/services/EmployeeService.java
+++ src/main/java/com/example/companydatagateway/services/EmployeeService.java
@@ -3,12 +3,40 @@ package com.example.companydatagateway.services;
 import com.example.companydatagateway.entities.Employee;
 import com.example.companydatagateway.models.EmployeeAttributesModel;
 import com.example.companydatagateway.models.EmployeePersonalInfoModel;
+import com.example.companydatagateway.repositories.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface EmployeeService {
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
 
-     List<Employee> getAllEmployees();
-     EmployeePersonalInfoModel getEmployeePersonalInfo(String id);
-     EmployeeAttributesModel getEmployeeAttributes(String id);
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public EmployeePersonalInfoModel getEmployeePersonalInfo(String id) {
+        Employee emp = employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee ID does not exist in Company Database"));
+        return new EmployeePersonalInfoModel(
+                emp.getId(), emp.getSsn(), emp.getFirstName(), emp.getLastName(), emp.getEmail());
+    }
+
+    public EmployeeAttributesModel getEmployeeAttributes(String id) {
+        Employee emp = employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee ID does not exist in Company Database"));
+
+        // Adjust attributes to be compatible with PACS
+        int yearsOfExperience = LocalDate.now().getYear() - emp.getContractStart().getYear() + 1;
+
+        return new EmployeeAttributesModel(
+                emp.getId(), emp.getRole(), emp.getDepartment(), emp.getTimeSchedule(),
+                emp.getClearanceLevel(), emp.getEmploymentStatus(), yearsOfExperience);
+    }
 }
